2e3d211e69ca71945a25e7f141863394de2e03fa
NicolasDele <nicolas.delevingne@etud.u-picardie.fr>
2023-03-22 19:20:31 +0100

    fonctionalité de main ajouter sur les boutns

--- app/src/main/java/com/example/mentalgame/MainActivity.java
+++ app/src/main/java/com/example/mentalgame/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.mentalgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button BtnPlay;
+    private Button BtnPerso;
+    private Button BtnAPropos;
+    private Button BtnScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnPlay = findViewById(R.id.button_play);
+        BtnPerso = findViewById(R.id.button_perso);
+        BtnAPropos = findViewById(R.id.button_a_propos);
+        BtnScore = findViewById(R.id.button_score);
+
+        BtnPlay.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,CalculActivity.class);
+            startActivity(intent);
+        });
+
+        BtnScore.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,ScoreActivity.class);
+            startActivity(intent);
+        });
+
+
+
+
     }
 }
