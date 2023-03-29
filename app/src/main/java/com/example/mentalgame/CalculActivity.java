b96f76d109a95cef3abb191ca82b3742dd0a9899
CorentinDelafosse <90545467+CorentinDelafosse@users.noreply.github.com>
2023-03-29 17:41:26 +0200

    CalculActivity interraction clavier

--- app/src/main/java/com/example/mentalgame/CalculActivity.java
+++ app/src/main/java/com/example/mentalgame/CalculActivity.java
@@ -3,30 +3,103 @@ package com.example.mentalgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class CalculActivity extends AppCompatActivity {
 
-    private String calcul = "";
+    private String Calcul = "";
     private Integer compteurTaille = 0;
     private TextView textViewCalcul;
+    private Button buttonOne;
+    private Button buttonDeux;
+    private Button buttonTrois;
+    private Button buttonQuatre;
+    private Button buttonCinq;
+    private Button buttonSix;
+    private Button buttonSept;
+    private Button buttonHuit;
+    private Button buttonNeuf;
+    private Button buttonZero;
+    private ImageButton buttonRetour;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcul);
+        textViewCalcul = findViewById(R.id.textcalcul);
+        buttonOne = findViewById(R.id.button_1);
+        buttonOne.setOnClickListener(view -> {
+            ajouterNombre("1");
+        });
+        buttonDeux = findViewById(R.id.button_2);
+        buttonDeux.setOnClickListener(view -> {
+            ajouterNombre("2");
+        });
+        buttonTrois = findViewById(R.id.button_3);
+        buttonTrois.setOnClickListener(view -> {
+            ajouterNombre("3");
+        });
+        buttonQuatre = findViewById(R.id.button_4);
+        buttonQuatre.setOnClickListener(view -> {
+            ajouterNombre("4");
+        });
+        buttonCinq = findViewById(R.id.button_5);
+        buttonCinq.setOnClickListener(view -> {
+            ajouterNombre("5");
+        });
+        buttonSix = findViewById(R.id.button_6);
+        buttonSix.setOnClickListener(view -> {
+            ajouterNombre("6");
+        });
+        buttonSept = findViewById(R.id.button_7);
+        buttonSept.setOnClickListener(view -> {
+            ajouterNombre("7");
+        });
+        buttonHuit = findViewById(R.id.button_8);
+        buttonHuit.setOnClickListener(view -> {
+            ajouterNombre("8");
+        });
+        buttonNeuf = findViewById(R.id.button_9);
+        buttonNeuf.setOnClickListener(view -> {
+            ajouterNombre("9");
+        });
+        buttonZero = findViewById(R.id.button_0);
+        buttonZero.setOnClickListener(view -> {
+            ajouterNombre("0");
+        });
+        buttonRetour = findViewById(R.id.button_retour);
+        buttonRetour.setOnClickListener(view -> {
+            suppressionNombre();
+        });
     }
 
-
-    private boolean ajouterCharactere(String charactereAAjouter) {
-        calcul += charactereAAjouter;
-        textViewCalcul.setText(calcul);
+    private boolean ajoutCharacter(String characterAjout){
+        Calcul += characterAjout;
+        textViewCalcul.setText(Calcul);
         return true;
     }
 
-    private boolean ajouterNombre(String nombre) {
-        ajouterCharactere(nombre);
+    private boolean ajouterNombre(String nombre){
+        compteurTaille++;
+        ajoutCharacter(nombre);
         return true;
     }
+
+    private boolean suppressionNombre(){
+        if(compteurTaille>0){
+            compteurTaille--;
+            Calcul = Calcul.substring(0, Calcul.length() - 1);
+            textViewCalcul.setText(Calcul);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
 }
